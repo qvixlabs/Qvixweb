5d07a28afb3e99b4ff845ea058cd5aab29624917
mazzwell <137079218+mazzwell@users.noreply.github.com>
2023-12-11 15:00:48 +0600

    api

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,27 +1,27 @@
-document.getElementById("signupForm").addEventListener("submit", function(event) {
+document.getElementById("signupForm").addEventListener("submit", async function(event) {
     event.preventDefault();
     
     const email = document.getElementById("emailInput").value;
-    const data = {
-        email_address: email,
-        status: "subscribed"
-    };
-
-    fetch('https://us11.api.mailchimp.com/3.0/lists/<list_id>/members', {
+    
+    try {
+      const response = await fetch('/api/subscribe', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + btoa('anystring:' + 'f6a13635cb26f9d9d37cbf327e5b0ec4-us11')
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (response.ok) {
-            alert("Successfully subscribed!");
-            // You can add further actions here after successful subscription
-        } else {
-            alert("Failed to subscribe. Please try again.");
-        }
-    })
-    .catch(error => console.error('Error:', error));
-});
+        body: JSON.stringify({ email })
+      });
+  
+      const result = await response.json();
+      if (response.ok) {
+        alert(result.message);
+        // You can add further actions here after successful subscription
+      } else {
+        alert(result.error || "Failed to subscribe. Please try again.");
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert("Failed to subscribe. Please try again.");
+    }
+  });
+  
