9b78c0342ba26980bc116d9922bfb3736a3663dc
mazzwell <137079218+mazzwell@users.noreply.github.com>
2023-12-11 15:05:46 +0600

    Update mail.js
    update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,27 +1,28 @@
 document.getElementById("signupForm").addEventListener("submit", async function(event) {
-    event.preventDefault();
-    
-    const email = document.getElementById("emailInput").value;
-    
-    try {
-      const response = await fetch('/api/subscribe', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email })
-      });
+  event.preventDefault();
   
-      const result = await response.json();
-      if (response.ok) {
-        alert(result.message);
-        // You can add further actions here after successful subscription
-      } else {
-        alert(result.error || "Failed to subscribe. Please try again.");
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      alert("Failed to subscribe. Please try again.");
+  const email = document.getElementById("emailInput").value;
+  
+  try {
+    const response = await fetch('/api/subscribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email })
+    });
+
+    if (response.ok) {
+      alert("Successfully subscribed!");
+      // You can add further actions here after successful subscription
+    } else {
+      const textResponse = await response.text();
+      alert(textResponse || "Failed to subscribe. Please try again.");
     }
-  });
+  } catch (error) {
+    console.error('Error:', error);
+    alert("Failed to subscribe. Please try again.");
+  }
+});
+
   
